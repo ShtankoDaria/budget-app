c6ea6f9a96ba5509212bc5d3ac95291ff77ac4aa
ShtankoDaria <daria.shtanko@gmail.com>
2020-04-17 15:50:15 +0200

    Show Balance method

--- javascript/app.js
+++ javascript/app.js
@@ -32,6 +32,30 @@ else {
   this.showBalance();
 }
 }
+
+//show balance method
+showBalance(){
+  const expense = this.totalExpense();
+  const total = parseInt(this.budgetAmount.textContent) - expense;
+  this.balanceAmount.textContent = total;
+  if (total <0){
+    this.balance.classList.remove("showGreen", "showBlack");
+    this.balance.classList.add("showRed");
+  }
+  else if (total >0){
+    this.balance.classList.remove("showRed", "showBlack");
+    this.balance.classList.add("showGreen");
+  }
+  else if (total >0){
+    this.balance.classList.remove("showRed", "showGreen");
+    this.balance.classList.add("showBlack");
+  }
+}
+
+//total expense
+totalExpense(){
+return total;
+}
 }
   
   function eventListeners(){
