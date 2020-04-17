1a84a3ed85ab1a2d20913f6b538d0543360ff4ec
ShtankoDaria <daria.shtanko@gmail.com>
2020-04-17 16:30:42 +0200

    Add expense done

--- javascript/app.js
+++ javascript/app.js
@@ -68,8 +68,8 @@ submitExpenseForm(){
   }
   else {
     let amount = parseInt(amountValue);
-    this.expenseInput = '';
-    this.amountInput ='';
+    this.expenseInput.value = '';
+    this.amountInput.value ='';
 
     let expense = {
       id: this.itemID,
@@ -84,6 +84,23 @@ submitExpenseForm(){
 }
 
 //add expense
+addExpense(expense){
+  const div = document.createElement('div');
+  div.classList.add('expense');
+  div.innerHTML = `<div class="expense-item d-flex justify-content-between align-items-baseline">
+  <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+  <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+  <div class="expense-icons list-item">
+   <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+    <i class="fas fa-edit"></i>
+   </a>
+   <a href="#" class="delete-icon" data-id="${expense.id}">
+    <i class="fas fa-trash"></i>
+   </a>
+  </div>
+ </div>`;
+ this.expenseList.appendChild(div);
+}
 
 
 //total expense
